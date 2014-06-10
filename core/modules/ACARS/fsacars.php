d487063a36655735c1a1be7530f794329e869d25
DavidJClark <david@noticeabledesigns.com>
2014-06-10 10:54:25 -0400

    Saving files before refreshing line endings

--- core/modules/ACARS/fsacars.php
+++ core/modules/ACARS/fsacars.php
@@ -7,24 +7,24 @@
  *
  * Interface for use with FSACARS
  * http://www.satavirtual.org/fsacars/
- * 
- * 
+ *
+ *
  * This file goes as this:
  *	The URL given is:
  *		<site>/index.php/acars/fsacars/<action>
- * 
+ *
  *  The action is set in the fsacas INI file:
- * 
+ *
  *		acars
  *		flightplan
  *		status
  *		pirep
- * 
+ *
  *  Pretty self-explanitory. I just check for the action ($_GET[action]),
  *	then follow the SDK docs to parse the message.
- * 
+ *
  *  There is a API for the ACARS, the ACARSData class.
- * 
+ *
  *  Anything inside the output buffering regions is thrown out
  *	unless debug = true in the function below
  */
@@ -37,7 +37,7 @@ ini_set('display_errors', 'off');
 Debug::log(serialize($_SERVER['QUERY_STRING']), 'fsacars');
 
 ##################################
-	
+
 # Our flight phase constants
 #	Don't change the order, the key is the # given by FSACARS
 
@@ -74,10 +74,10 @@ switch($acars_action)
 	#	Code is here but currently not implemented
 	#	or tested
 	#
-	
+
 	case 'acars':
 	case 'fsacars':
-		
+
 		Debug::log('ACARS UPDATE', 'fsacars');
 		Debug::log(print_r($_GET, true), 'fsacars');
 
@@ -89,26 +89,26 @@ switch($acars_action)
 			$code = $matches[1];
 			$pilotid = intval($matches[2]) - Config::Get('PILOTID_OFFSET');
 		}*/
-		
+
 		$fields = array('pilotid'=>$pilotid,
-						'messagelog'=>str_ireplace('Message content: £', '', $_GET['mcontent']).'\n');
-		
+						'messagelog'=>str_ireplace('Message content: ï¿½', '', $_GET['mcontent']).'\n');
+
 		ob_start();
 		ACARSData::updateFlightData($fields['pilotid'], $fields);
-		
+
 		$cont = ob_get_clean();
-		
+
 		ob_end_clean();
-		
+
 		Debug::log($cont, 'fsacars');
-		
+
 		break;
-		
+
 	case 'flightplans':
 	case 'schedules':
-	
+
 		Debug::log('FLIGHT PLAN REQUEST', 'fsacars');
-		
+
 		/*if(!is_numeric($_GET['pilot']))
 		{
 			# see if they are a valid pilot:
@@ -119,7 +119,7 @@ switch($acars_action)
 		$pilotid = PilotData::parsePilotID($_GET['pilot']);
 		$route = SchedulesData::getLatestBid($pilotid);
 		$date = date('Y:m:d');
-		
+
 		# Get load counts
 		if($route->flighttype=='H')
 		{
@@ -136,9 +136,9 @@ switch($acars_action)
 				$maxpax = FinanceData::GetLoadCount($route->aircraftid, 'P');
 			}
 		}
-		
-		//$starttime = 
-		
+
+		//$starttime =
+
 echo "OK
 $route->depicao
 $route->arricao
@@ -154,22 +154,22 @@ $route->route
 
 
 $maxpax
-$maxcargo";	
+$maxcargo";
 		break;
-	
+
 	#
 	# Position Update
 	#
 	case 'status':
-	
+
 		Debug::log('STATUS UPDATE', 'fsacars');
 		Debug::log(print_r($_GET, true), 'fsacars');
-		
+
 		if($_GET['detailph']=='')
 		{
 			# Vary our detail phase based on the general phase if none is supplied
 			#	Depending on the FSACARs version
-			
+
 			if($_GET['Ph'] == 1)
 				$_GET['detailph'] = 1;
 			elseif($_GET['Ph'] == 2)
@@ -181,7 +181,7 @@ $maxcargo";
 			else
 				$_GET['detailph'] = 1;
 		}
-		
+
 		/*if(!is_numeric($_GET['pnumber']))
 		{
 			# see if they are a valid pilot:
@@ -189,9 +189,9 @@ $maxcargo";
 			$code = $matches[1];
 			$pilotid = intval($matches[2]) - Config::Get('PILOTID_OFFSET');
 		}*/
-		
+
 		$pilotid = PilotData::parsePilotID($_GET['pnumber']);
-		
+
 		$ac = OperationsData::GetAircraftByReg($_GET['Regist']);
 		if(!$ac)
 		{
@@ -202,7 +202,7 @@ $maxcargo";
 			$aircraft = $ac->id;
 			unset($ac);
 		}
-		
+
 		$fields = array(
 			'pilotid'=>$pilotid,
 			'flightnum'=>$_GET['IATA'],
@@ -229,22 +229,22 @@ $maxcargo";
 		);
 
 		Debug::log(print_r($fields, true), 'fsacars');
-		
+
 		ACARSData::updateFlightData($pilotid, $fields);
-		
+
 		Debug::log($cont, 'fsacars');
-		
+
 		echo 'OK';
 		break;
-	
+
 	#
 	# File the PIREP
 	#
 	case 'pirep':
-	
+
 		Debug::log('PIREP FILE', 'fsacars');
 		Debug::log(serialize($_GET), 'fsacars');
-		
+
 		$pilotid = PilotData::parsePilotID($_GET['pilot']);
 
 		/*if(is_numeric($_GET['pilot']))
@@ -264,69 +264,69 @@ $maxcargo";
 			echo 'Invalid Pilot!';
 			return;
 		}
-				
+
 		#
 		# Check if anything was in the log
 		#	If not, then it probably wasn't a multi-chunk, so
 		#	 just pull it straight from the query string
 		#	Otherwise, pull the full-text from the session
 		#
-		
+
 		if($_GET['more'] == '1')
 		{
 			#
 			# We have more coming to the log
 			#
 			$report = PIREPData::GetLastReports($pilotid, 1);
-			
+
 			/* Check for any other data which might be in the other
 				chunks sent by fsacars, because it's stupid and doesn't
 				just do POST */
 			$log = explode('*', $_GET['log']);
-			
+
 			/* Find the landing rate */
 			$pos = find_in_fsacars_log('TouchDown:Rate', $log);
 			$landingrate = str_replace('TouchDown:Rate', '', $log[$pos]);
 			$count = preg_match('/([0-9]*:[0-9]*).*([-+]\d*).*/i', $landingrate, $matches);
-			
+
 			if($count > 0)
 			{
 				PIREPData::editPIREPFields($report->pirepid, array('landingrate' => $matches[2]));
 			}
-			
+
 			PIREPData::AppendToLog($report->pirepid, $_GET['log']);
-						
+
 			echo 'OK';
 			return;
 		}
-		
+
 		# Full PIREP, run with it
 		preg_match('/^([A-Za-z]*)(\d*)/', $_GET['pilot'], $matches);
 		$code = $matches[1];
-		
+
 		$log = explode('*', $_GET['log']);
-		
+
 		# Find where flight IATA is
 		# And extract the code and flight number
 		$pos = find_in_fsacars_log('Flight IATA', $log);
 		$flightnum = str_replace('Flight IATA:', '', $log[$pos]);
 		//preg_match('/^([A-Za-z]*)(\d*)/', $flightnum, $matches);
-		
+
 		$flightinfo = SchedulesData::getProperFlightNum($flightnum);
 		$code = $flightinfo['code'];
 		$flightnum = $flightinfo['flightnum'];
-		
+
 		# Get the passenger count:
 		# Find where flight IATA is
 		$pos = find_in_fsacars_log('PAX', $log);
 		$load = str_replace('PAX:', '', $log[$pos]);
-		
+
 		$pos = find_in_fsacars_log('TouchDown:Rate', $log);
 		$landingrate = str_replace('TouchDown:Rate', '', $log[$pos]);
-		
+
 		$pos = find_in_fsacars_log('Route', $log);
 		$route = str_replace('Route:', '', $log[$pos]);
-		
+
 		$count = preg_match('/([0-9]*:[0-9]*).*([-+]\d*).*/i', $landingrate, $matches);
 		if($count > 0)
 		{
@@ -336,53 +336,53 @@ $maxcargo";
 		{
 			$landingrate = 0;
 		}
-		
+
 		# Get our aircraft
 		$reg = trim($_GET['reg']);
 		$ac = OperationsData::GetAircraftByReg($reg);
-				
+
 		# Do some cleanup
 		$_GET['origin'] = DB::escape($_GET['origin']);
 		$_GET['dest'] = DB::escape($_GET['dest']);
-		
+
 		# Get schedule info, using minimal information
 		#	Check if they forgot the flight code
 		if($code == '')
 		{
 			# Find a flight using just the flight code
 			$sched = SchedulesData::FindFlight($flightnum);
-		
+
 			# Can't do it. They completely fucked this up
 			if(!$sched)
 			{
 				return;
 			}
-			
+
 			$code = $sched->code;
 			$flightnum = $sched->flightnum;
-			
+
 			if($_GET['origin'] != $sched->depicao || $_GET['dest'] != $sched->arricao)
 			{
 				$comment = 'phpVMS Message: Arrival or Departure does not match schedule';
 			}
 		}
-			
-		
+
+
 		# Make sure airports exist:
 		#  If not, add them.
 		if(!OperationsData::GetAirportInfo($_GET['origin']))
 		{
 			OperationsData::RetrieveAirportInfo($_GET['origin']);
 		}
-		
+
 		if(!OperationsData::GetAirportInfo($_GET['dest']))
 		{
 			OperationsData::RetrieveAirportInfo($_GET['dest']);
 		}
-		
-		# Convert the time to xx.xx 
+
+		# Convert the time to xx.xx
 		$flighttime = number_format(floatval(str_replace(':', '.', $_GET['duration'])), 2);
-		
+
 		$data = array('pilotid' => $pilotid,
 						'code' => $code,
 						'flightnum' => $flightnum,
@@ -399,9 +399,9 @@ $maxcargo";
 						'load' => $load,
 						'rawdata' => $log,
 						'log'=> $_GET['log']);
-		
+
 		$ret = ACARSData::FilePIREP($pilotid, $data);
-			
+
 		echo 'OK';
 		break;
 }
