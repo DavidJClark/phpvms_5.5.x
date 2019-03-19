5bbc91b792e1e57f9798258397f957f958207884
ProAviaAZ <24687699+ProAviaAZ@users.noreply.github.com>
2019-03-18 19:56:08 -0700

    Update sidebar_dashboard.php
    Code corrections for Countable

--- admin/templates/sidebar_dashboard.php
+++ admin/templates/sidebar_dashboard.php
@@ -8,11 +8,20 @@
 if(PilotGroups::group_has_perm(Auth::$usergroups, MODERATE_PIREPS)) 
 {
 ?>
+<?php
+}
 if (is_array(PIREPData::GetAllReportsByAccept(PIREP_PENDING)) || (PIREPData::GetAllReportsByAccept(PIREP_PENDING)) instanceof Countable)
+{
+?>
 <strong><a href="<?php echo SITE_URL?>/admin/index.php/pirepadmin/viewpending"><?php echo  count(PIREPData::GetAllReportsByAccept(PIREP_PENDING))?></strong> PIREPs pending</a><br />
 <?php 
 }
 if(PilotGroups::group_has_perm(Auth::$usergroups, MODERATE_REGISTRATIONS)) 
+{
+?>
+<?php
+}
+if (is_array(PilotData::GetPendingPilots()) || (PilotData::GetPendingPilots()) instanceof Countable)
 {
 ?>
 <strong><a href="<?php echo SITE_URL?>/admin/index.php/pilotadmin/pendingpilots"><?php echo  count(PilotData::GetPendingPilots())?></strong> Pilot registrations pending</a>
